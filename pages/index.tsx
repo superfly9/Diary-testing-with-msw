569b9db504f407ebb26ea54ee9717347bc85b7c5
송승찬 <superfly09@daum.net>
2022-10-20 03:25:26 +0900

    feat: 일기 리스트 정렬 함수(날짜 / 감정) 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,88 +7,139 @@ import List from "../components/List";
 import Menu from "../components/Menu";
 import { DateType, Diary, DirayList, EmotionType } from "../types/home";
 
-const DATE_FILTERS = [{
-  value :'latest',
-  name  :'최신순',
-},{
-  value :'oldest',
-  name  :'오래된 순',
-}];
-const EMOTION_FILTERS = [{
-  value:'all',
-  name:'전부 다'
-},{
-  value:'good',
-  name :'좋은 감정만'
-},{
-  value:'bad',
-  name:'나쁜 감정만'
-}];
-
+const DATE_FILTERS = [
+  {
+    value: "latest",
+    name: "최신순",
+  },
+  {
+    value: "oldest",
+    name: "오래된 순",
+  },
+];
+const EMOTION_FILTERS = [
+  {
+    value: "all",
+    name: "전부 다",
+  },
+  {
+    value: "good",
+    name: "좋은 감정만",
+  },
+  {
+    value: "bad",
+    name: "나쁜 감정만",
+  },
+];
 
 const Home: NextPage<DirayList> = ({ list }) => {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [diary, setDiary] = useState<Diary [] | []>([])
+  const [diary, setDiary] = useState<Diary[]>([]);
 
-  const currentYear = useMemo(()=>currentDate.getFullYear(), [currentDate]);
-  const currentMonth = useMemo(()=>currentDate.getMonth() , [currentDate]);
-  const startOfThisMonth = useMemo(()=>new Date(currentYear, currentMonth , 1).getTime(), [currentDate]);
-  const startOfNextMonth = useMemo(()=> new Date(currentYear, currentMonth+1, 1).getTime() , [currentDate]);
+  const currentYear = useMemo(() => currentDate.getFullYear(), [currentDate]);
+  const currentMonth = useMemo(() => currentDate.getMonth(), [currentDate]);
+  const startOfThisMonth = useMemo(
+    () => new Date(currentYear, currentMonth, 1).getTime(),
+    [currentDate]
+  );
+  const startOfNextMonth = useMemo(
+    () => new Date(currentYear, currentMonth + 1, 1).getTime(),
+    [currentDate]
+  );
 
-  const monthChangeHandler = useCallback((type : 'prev' | 'next')=>{
-    const date = new Date(currentYear,  type === 'prev' ? currentMonth -1 : currentMonth + 1 ,1)
-    setCurrentDate(date)
-  }, [currentDate])
-  
+  const monthChangeHandler = useCallback(
+    (type: "prev" | "next") => {
+      const date = new Date(
+        currentYear,
+        type === "prev" ? currentMonth - 1 : currentMonth + 1,
+        1
+      );
+      setCurrentDate(date);
+    },
+    [currentDate]
+  );
+
   const headerTitle = useMemo(
     () => `${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월`,
     [currentDate]
   );
+  const [dateType, setDateType] = useState<DateType>("latest");
+  const [emotionType, setEmotionType] = useState<EmotionType>("all");
 
-  useEffect(()=>{
-    const filteredList= list.filter(v=>{
-      const createdAt = new Date(v.createdAt).getTime();
-      return createdAt >=startOfThisMonth && createdAt < startOfNextMonth
-    })
-    setDiary(filteredList)
-  },[currentDate])
+  const filteredDataByEmotion = (arr: Diary[]) => {
+    if (emotionType === "all") return arr;
+    if (emotionType === "good") return arr.filter((v) => v.emotion <= 3);
+    if (emotionType === "bad") return arr.filter((v) => v.emotion > 3);
+  };
 
-
-  const [dataType, setDateType] = useState<DateType | string>('latest');
-  const [emotionType, setEmotionType] = useState<EmotionType | string>('all');
-
-  const filterHandler = (e:ChangeEvent<HTMLSelectElement>)=>{
-    const value = e.target.value;
-    const name = e.target.name;
-    switch (name) {
-      case 'Date':
-        setDateType(value)
-        case 'Emotion':
-        setEmotionType(value);
-      default:
-        break;
+  const filterDataByDate = (list: Diary[]) => {
+    let deepCopiedData: Diary[] = JSON.parse(JSON.stringify(list));
+    if (dateType === "latest") {
+      deepCopiedData.sort(
+        (a, b) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      );
+    } else {
+      deepCopiedData.sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+      );
     }
-  }
+    return deepCopiedData;
+  };
+
+  useEffect(() => {
+    const getThisMonthDiary = list.filter((v) => {
+      const createdAt = new Date(v.createdAt).getTime();
+      return createdAt >= startOfThisMonth && createdAt < startOfNextMonth;
+    });
+    const getFilterdData = filteredDataByEmotion(
+      filterDataByDate(getThisMonthDiary)
+    );
+    setDiary(getFilterdData!!);
+  }, [currentDate, dateType, emotionType]);
 
   const MenuLeftChild = (
     <>
-    <select onChange={filterHandler} name='Date' className='controlMenu'>
-        {DATE_FILTERS.map(({value,name})=><option key={value}>{name}</option>)}
+      <select
+        onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+          setDateType(e.target.value as DateType)
+        }
+        name="Date"
+        className="controlMenu"
+      >
+        {DATE_FILTERS.map(({ value, name }) => (
+          <option key={value} value={value}>
+            {name}
+          </option>
+        ))}
       </select>
-      <select onChange={filterHandler} name='Emotion' className='controlMenu'>
-        {EMOTION_FILTERS.map(({value,name})=><option key={value}>{name}</option>)}
+      <select
+        onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+          setEmotionType(e.target.value as EmotionType)
+        }
+        name="Emotion"
+        className="controlMenu"
+      >
+        {EMOTION_FILTERS.map(({ value, name }) => (
+          <option key={value} value={value}>
+            {name}
+          </option>
+        ))}
       </select>
     </>
-  )
-  const MenuRightChild = <Link href='/new'>새 일기 쓰기</Link>
-  
+  );
+  const MenuRightChild = <Link href="/new">새 일기 쓰기</Link>;
+
   return (
-    <Layout 
+    <Layout
       text={headerTitle}
-      LeftChild={<Button text="<" onClick={()=>monthChangeHandler('prev')} />}
-      RightChild={<Button text=">" onClick={()=>monthChangeHandler('next')} />}
+      LeftChild={<Button text="<" onClick={() => monthChangeHandler("prev")} />}
+      RightChild={
+        <Button text=">" onClick={() => monthChangeHandler("next")} />
+      }
     >
-      <Menu LeftChild={MenuLeftChild} RightChild={MenuRightChild} />        
+      <Menu LeftChild={MenuLeftChild} RightChild={MenuRightChild} />
       <List list={diary} />
     </Layout>
   );
