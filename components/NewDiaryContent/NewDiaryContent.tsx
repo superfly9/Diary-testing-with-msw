090761ffe44fab93d7d793ce65bd704ab855c8db
송승찬 <superfly09@daum.net>
2022-10-19 22:53:38 +0900

    feat: 일기 작성 페이지 마크업 추가

--- components/NewDiaryContent/NewDiaryContent.tsx
+++ components/NewDiaryContent/NewDiaryContent.tsx
@@ -1,14 +1,38 @@
+import { useRouter } from "next/router";
 import React from "react";
+import Button from "../Button";
+import EmotionItem from "../EmotionItem";
 import styles from "./NewDiaryContent.module.css";
 
 function NewDiaryContent() {
+  const router = useRouter();
+  const diaryCompleteConfirmHandler =()=>{
+    if (window.confirm('일기를 발행하시겠습니까?')) {
+      // 일기 저장 함수 추가 로직 추가 예정
+      router.back()
+    } 
+  }
   return (
     <div className={styles.wrapper}>
       <div>
         <em className={styles.tit}>오늘은 언제인가요?</em>
-        <input type="date" />
+        <input
+          type="date"
+          value={new Date().toISOString().slice(0, 10)}
+          onChange={e=>console.log(e.target.value)}
+          className={styles.date}
+        />
       </div>
-      <em>오늘의 감정</em>
+      <em className={styles.tit}>오늘의 감정</em>
+      <EmotionItem />
+      <em className={styles.tit}>오늘의 일기</em>
+      <textarea 
+        className={styles.textarea}
+      /> 
+      <div className={styles.btn_wrapper}>
+        <Button text='취소하기' onClick={()=>router.back()} />
+        <Button text='발행하기' onClick={diaryCompleteConfirmHandler} type='positive' />
+      </div> 
     </div>
   );
 }
--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,19 +1,18 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 import Button from '../../components/Button'
-import EmotionItem from '../../components/EmotionItem'
 import Layout from '../../components/Layout'
 import NewDiaryContent from '../../components/NewDiaryContent'
 
 function NewDiary() {
     const router = useRouter()
   return (
+
     <Layout 
         text='새 일기쓰기'
         LeftChild={<Button text='< 뒤로가기' onClick={()=>router.back()} />}
     >
     <NewDiaryContent />
-    <EmotionItem />
     </Layout>
   )
 }
