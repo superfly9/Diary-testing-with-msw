803d44c283dd14a6c163462bf9e14498816a39d5
송승찬 <superfly09@daum.net>
2022-12-03 15:49:42 +0900

    feat: 스켈레톤 로딩 라이브러리로 수정

--- components/DetailContent/index.tsx
+++ components/DetailContent/index.tsx
@@ -1,64 +1,109 @@
 import Image from "next/image";
 import React, { MouseEvent, useState } from "react";
 import styles from "./DetailContent.module.css";
-import { EMOTION_LIST } from '../../constants/emotion_list';
+import { EMOTION_LIST } from "../../constants/emotion_list";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
 
 interface Props {
-  content:string;
-  emotion:number;
+  loading: boolean;
+  content: string;
+  emotion: number;
   likeCount: number;
 }
 const LIKE_UNIT = 10000;
 
-function DetailContent({ content, emotion,likeCount }: Props) {
+function DetailContent({ loading, content, emotion, likeCount }: Props) {
   const { emotionName } = EMOTION_LIST.find((v) => v.value === emotion) || {
     emotionName: "감정을 알  수 없는 상태입니다.",
   };
   const IMAGE_URL = `/emotion_status/emotion${emotion}.png`;
 
-  const [like,setLike] = useState(false);
-  const [dislike,setDisLike] = useState(false);
+  const [like, setLike] = useState(false);
+  const [dislike, setDisLike] = useState(false);
 
-  const dividedLikeByUnit = likeCount < 10000 ? likeCount : `${(likeCount / LIKE_UNIT).toFixed(1)}만`;
+  const dividedLikeByUnit =
+    likeCount < 10000 ? likeCount : `${(likeCount / LIKE_UNIT).toFixed(1)}만`;
 
-  const likeOrDisLikeClickHandler = (e:MouseEvent<HTMLButtonElement>)=>{
+  const likeOrDisLikeClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
     const type = e.currentTarget.dataset.type;
-    if (type === 'like') {
-      setLike(v=>!v)
-      setDisLike(false)
-    } 
-    if (type === 'dislike') {
-      setDisLike(v=>!v)
-      setLike(false)
+    if (type === "like") {
+      setLike((v) => !v);
+      setDisLike(false);
     }
-  }
-  const likeClassName = [styles.btn, styles.like, like ? styles.active :'' ].join(" ");
-  const disLikeClassName =[styles.btn, styles.dislike, dislike ? styles.active :''].join(" ");
+    if (type === "dislike") {
+      setDisLike((v) => !v);
+      setLike(false);
+    }
+  };
+  const likeClassName = [
+    styles.btn,
+    styles.like,
+    like ? styles.active : "",
+  ].join(" ");
+  const disLikeClassName = [
+    styles.btn,
+    styles.dislike,
+    dislike ? styles.active : "",
+  ].join(" ");
 
   return (
     <>
       <div className={styles.emotion_wrapper}>
-        <em className={styles.bold_txt}>오늘의 감정</em>
-        <div className={[styles.emotion_content, styles[`status_${emotion}`]].join(' ')}>
-          <Image src={IMAGE_URL} alt={`감정_${emotion}`} width={140} height={140} />
-          <span className={styles.emotion_txt}>{emotionName}</span>
-        </div>
+        {loading ? (
+          <Skeleton />
+        ) : (
+          <>
+            <em className={styles.bold_txt}>오늘의 감정</em>
+            <div
+              className={[
+                styles.emotion_content,
+                styles[`status_${emotion}`],
+              ].join(" ")}
+            >
+              <Image
+                src={IMAGE_URL}
+                alt={`감정_${emotion}`}
+                width={140}
+                height={140}
+              />
+              <span className={styles.emotion_txt}>{emotionName}</span>
+            </div>
+          </>
+        )}
       </div>
 
       <div className={styles.content_wrapper}>
-        <em className={styles.bold_txt}>오늘의 일기</em>
-        <div className={styles.content}>
-          <p>{content}</p>
-        </div>
+        {loading ? (
+          <Skeleton />
+        ) : (
+          <>
+            <em className={styles.bold_txt}>오늘의 일기</em>
+            <div className={styles.content}>
+              <p>{content}</p>
+            </div>
+          </>
+        )}
       </div>
       <div className={styles.btn_wrapper}>
-          <button data-type='like' onClick={likeOrDisLikeClickHandler} className={likeClassName}>
-            <span className={styles.count}>{dividedLikeByUnit}</span>
-            <span className={styles.blind}>좋아요 {dividedLikeByUnit}</span>
-          </button>
-          <button data-type='dislike' onClick={likeOrDisLikeClickHandler} className={disLikeClassName}>
-          <span className={styles.blind}>싫어요</span>
-          </button>
+          {loading ? <Skeleton /> :
+          <>
+            <button
+              data-type="like"
+              onClick={likeOrDisLikeClickHandler}
+              className={likeClassName}
+            >
+              <span className={styles.count}>{dividedLikeByUnit}</span>
+              <span className={styles.blind}>좋아요 {dividedLikeByUnit}</span>
+            </button>
+            <button
+              data-type="dislike"
+              onClick={likeOrDisLikeClickHandler}
+              className={disLikeClassName}
+            >
+              <span className={styles.blind}>싫어요</span>
+            </button>
+          </>}
       </div>
     </>
   );
--- pages/detail/[diaryId].tsx
+++ pages/detail/[diaryId].tsx
@@ -7,7 +7,9 @@ import { Diary } from "../../types/home";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, doc, getDoc } from "firebase/firestore";
 import { database } from "../../firebase/config";
-import SkeletonDetail from "../../skeleton/detail/SkeletonDetail";
+
+const ERROR_REMAINING_TIME = 3000;
+const CONTENT_DEFAULT = { createdAt : new Date(), content:'', emotion:3,likeCount:0}
 
 function DetailDiary() {
   const router = useRouter();
@@ -17,9 +19,7 @@ function DetailDiary() {
     collection(database, "diaryLists"),
     {}
   );
-  const [detailContent, setDetailContent] = useState<Omit<Diary, "id"> | null>(
-    null
-  );
+  const [detailContent, setDetailContent] = useState<Omit<Diary, "id"> | null>(null);
 
   const getDocs = async () => {
     const docRef = doc(database, "diaryLists", diaryId);
@@ -45,17 +45,26 @@ function DetailDiary() {
   };
 
   useEffect(() => {
-    if (!loading && diary) {
+    if (!loading) {
       getDocs();
     }
   }, [loading, diaryId]);
   const goToPrev = () => router.back();
   const goToEdit = () => router.push(`/edit/${diaryId}`);
 
-  if (!detailContent || loading) return <SkeletonDetail />
-  if (error) return <p>잠시 후 다시 시도해주세요!</p>;
+  if (error) {
+  setTimeout(()=>{
+    goToPrev()
+  },ERROR_REMAINING_TIME)
+  return (
+    <>
+      <p>잠시 후 다시 시도해주세요!</p>
+      <span>3초 뒤 홈으로 이동합니다</span>
+    </>
+  )
+}
 
-  const { createdAt, content, emotion, likeCount } = detailContent;
+  const { createdAt, content, emotion, likeCount } = detailContent || CONTENT_DEFAULT
   const headerTxt = `${new Date(createdAt).getFullYear()}년 
   ${new Date(createdAt).getMonth() + 1}월 ${new Date(createdAt).getDate()}일`;
 
@@ -66,6 +75,7 @@ function DetailDiary() {
       RightChild={<Button text="수정하기" onClick={goToEdit} />}
     >
       <DetailContent
+        loading
         content={content}
         emotion={emotion}
         likeCount={likeCount}
